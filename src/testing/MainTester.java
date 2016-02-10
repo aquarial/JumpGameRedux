366f8457e3d9d74a5b68c7b7a76ab0c7c27965d5
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-10 13:21:30 -0600

    Intialize jframe

--- src/testing/MainTester.java
+++ src/testing/MainTester.java
@@ -1,10 +1,19 @@
 package testing;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class MainTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame f = new JFrame("Game Test Frame");
+				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				f.setSize(600, 400);
+				f.setVisible(true);
+			}
+		});
 	}
 
 }
