1cb53b639589097c98ba540de5549778f4090468
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-10 15:36:58 -0600

    Creates Resources.java
    Functions that allow me to easily load xml data from resouce files

--- src/graphics/JPanelConstructor.java
+++ src/graphics/JPanelConstructor.java
@@ -1,14 +1,18 @@
 package graphics;
 
+import io.Resources;
+
 import java.awt.Color;
 
 import javax.swing.JPanel;
 
 public class JPanelConstructor {
 	public static JPanel makeNewGamePanel() {
+		Resources r = new Resources();
+		r.getDocumentForLevel(1);
+
 		JPanel pan = new JPanel();
 		pan.setBackground(Color.BLUE);
 		return pan;
-
 	}
 }
---
+++ src/io/Resources.java
@@ -0,0 +1,39 @@
+package io;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class Resources {
+
+	public void getDocumentForLevel(int level) {
+		try {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder;
+			dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(getInputFile(level));
+			doc.getDocumentElement().normalize();
+			System.out.println("Root element :"
+					+ doc.getDocumentElement().getNodeName());
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public File getInputFile(int level) {
+		Class classloader = Thread.currentThread().getClass();
+
+		return new File(classloader.getResource("/levels/001.xml").getPath());
+	}
+}
