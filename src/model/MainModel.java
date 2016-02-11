b3f5fc0fa67a02d66a9d7e07e805972cb1a23ec1
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-10 22:26:01 -0600

    Read on...
    Every tick the jumper will move some distance. It might cross over onto
    a Quad obstical. 
    
    The methods I have been adding and removing are to aid in calcuating how
    to fix these kinds of crosses before they happen. 
    
    The strategy is to calculate where the next tick will move the jumper,
    and see if it intersects any known obsticle.

--- src/model/MainModel.java
+++ src/model/MainModel.java
@@ -3,6 +3,34 @@ package model;
 import java.util.List;
 
 public class MainModel {
-	
-	private List<Quad> currentObstacles;
+
+	private List<Quad> stickyBlocks;
+	private Jumper jumper;
+
+	public MainModel(Point playerLocation, List<Quad> obsticales) {
+		this.jumper = new Jumper(playerLocation);
+		this.stickyBlocks = obsticales;
+	}
+
+	public void update(float deltaTime) {
+		Point newPosition = jumper.calculateUpdate(deltaTime);
+
+		Quad jumperQuad = Jumper.calculateQuadAtPosition(newPosition);
+		Point[] corners = jumperQuad.toPointArray();
+
+		Point[] newJumperPositions = new Point[4];
+		for (int index = 0; index < 4; index++) {
+			Point corner = corners[index];
+			
+		}
+	}
+
+	public float jumperXPos() {
+		return jumper.getXposition();
+	}
+
+	public float jumperYPos() {
+		return jumper.getYposition();
+	}
+
 }
