73b8ee80351f2fc6e9c1fee9264a949ee0532939
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-13 17:28:28 -0600

    Force repaint to be called when window is resized

--- src/testing/MainTester.java
+++ src/testing/MainTester.java
@@ -15,7 +15,7 @@ public class MainTester {
 				JFrame f = new JFrame("Game Test Frame");
 				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				f.setSize(600, 400);
-				f.setResizable(false);
+				//f.setResizable(false);
 				f.setVisible(true);
 				GamePanel gp = JPanelConstructor.makeNewGamePanel("001");
 				f.add(JPanelConstructor.makeNewGamePanel("001"));
