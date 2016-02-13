690b2824ab219bab65e6a85e5beba12c84d6753f
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-12 21:26:05 -0600

    Allows me to compare Points

---
+++ src/model/PointComparator.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Comparator;
+
+public class PointComparator implements Comparator<Point> {
+
+	@Override
+	public int compare(Point p1, Point p2) {
+		return Double.compare(Math.pow(p1.getX(), 2) + Math.pow(p1.getY(), 2),
+				Math.pow(p2.getX(), 2) + Math.pow(p2.getY(), 2));
+	}
+
+}
