47302167dc8bc61ca067c5779a3fa0cc2fa0ce3c
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-13 14:53:35 -0600

    Adds constructor

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -6,10 +6,16 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import model.MainModel;
+
 public class GamePanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-
+	private MainModel model;
+	public GamePanel(MainModel m) {
+		super();
+		model = m;
+	}
 	public void paint(Graphics g) {
 		System.out.println("print");
 		Graphics2D g2 = (Graphics2D) g;
@@ -17,5 +23,6 @@ public class GamePanel extends JPanel {
 		g2.setColor(Color.BLACK);
 
 		g2.fillRect(10, 10, 40, 70);
+		
 	}
 }
