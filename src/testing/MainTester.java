5f42e503ac2d7ebe5861ea135af28d76e75830b3
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-12 20:00:09 -0600

    Better test state

--- src/testing/MainTester.java
+++ src/testing/MainTester.java
@@ -6,13 +6,15 @@ public class MainTester {
 
 	public static void main(String[] args) {
 
-		Quad q1 = new Quad(1, 2, 3, -1);
-		Quad q2 = new Quad(0, 1, 2, 0);
+		Quad q1 = new Quad(2, 1, 5, 3);
+		Quad q2 = new Quad(2, 4, 3, 5);
 
-		for (double ang = -Math.PI; ang < Math.PI; ang += Math.PI / 8) {
-			System.out.print(ang / Math.PI + " ");
-			q1.removeJumperFromThis(q2, 1, 0);
-		}
+		q1.removeJumperFromThis(q2, 1, -1);
+		//
+		// for (double ang = -Math.PI; ang < Math.PI; ang += Math.PI / 8) {
+		// System.out.print(ang / Math.PI + " ");
+		// q1.removeJumperFromThis(q2, 1, 0);
+		// }
 
 		// SwingUtilities.invokeLater(new Runnable() {
 		// public void run() {
