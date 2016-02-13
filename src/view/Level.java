12ae7c3d8af83df6ceef2edc12c6ee3d497038de
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-13 13:25:36 -0600

    Adds very usefull xml methods

--- src/view/Level.java
+++ src/view/Level.java
@@ -27,7 +27,38 @@ public class Level {
 		System.out.println("x = " + p_loc_x.getNodeValue());
 		System.out.println("y = " + p_loc_y.getNodeValue());
 
-		Node quads = nodes.item(2);
+	void printNodes(String name, NodeList nodes) {
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node n = nodes.item(i);
+			System.out.println(name + "[" + i + "]" + " = " + n.getNodeName());
+		}
+	}
+
+	/**
+	 * src="http://www.sitepoint.com/removing-useless-nodes-from-the-dom/"
+	 * 
+	 * Removes whitespace and #text nodes
+	 * 
+	 * @param node Node to clean
+	 */
+	private void clean(Node node) {
+		NodeList childNodes = node.getChildNodes();
+
+		for (int n = childNodes.getLength() - 1; n >= 0; n--) {
+			Node child = childNodes.item(n);
+			short nodeType = child.getNodeType();
+
+			if (nodeType == Node.ELEMENT_NODE)
+				clean(child);
+			else if (nodeType == Node.TEXT_NODE) {
+				String trimmedNodeVal = child.getNodeValue().trim();
+				if (trimmedNodeVal.length() == 0)
+					node.removeChild(child);
+				else
+					child.setNodeValue(trimmedNodeVal);
+			} else if (nodeType == Node.COMMENT_NODE)
+				node.removeChild(child);
+		}
 	}
 
 }
