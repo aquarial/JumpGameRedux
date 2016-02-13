2417046903c4dc6f936686dc913b696707207301
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-13 17:04:48 -0600

    Pass out GamePanel, not JPanel.
    Important because JPanel doesnt have a "start" method to override

--- src/view/JPanelConstructor.java
+++ src/view/JPanelConstructor.java
@@ -6,12 +6,12 @@ import model.Level;
 import model.MainModel;
 
 public class JPanelConstructor {
-	public static JPanel makeNewGamePanel(String levelname) {
+	public static GamePanel makeNewGamePanel(String levelname) {
 
 		Level level = new Level(levelname);
 		MainModel m = new MainModel(level);
 
-		JPanel pan = new GamePanel(m);
+		GamePanel pan = new GamePanel(m);
 		return pan;
 	}
 }
