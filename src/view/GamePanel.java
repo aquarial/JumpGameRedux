f0260d610d995ef8db0acf8ced166bca400e1a2c
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-13 16:02:23 -0600

    Step 02 - Inverse the y coordinates
    Graphics2D will draw from the top left, but the model y position is how
    far it is from the bottom left. 
    
    To fix this, we multiply the relative y positions by negative 1, or flip
    the order of the subtraction.

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -38,9 +38,9 @@ public class GamePanel extends JPanel {
 
 	private void drawStickyQuad(Quad stickyQuad, Graphics2D g2) {
 		double x1 = (stickyQuad.getX1() - model.getPlayerXPos());
-		double y1 = (stickyQuad.getY1() - model.getPlayerYPos());
+		double y1 = (model.getPlayerYPos() - stickyQuad.getY1());
 		double x2 = (stickyQuad.getX2() - model.getPlayerXPos());
-		double y2 = (stickyQuad.getY2() - model.getPlayerYPos());
+		double y2 = (model.getPlayerYPos() - stickyQuad.getY2());
 		System.out.println("draw quad at " + x1 + " " + y1 + " " + x2 + " " + y2);
 		// g2.drawRect(x1, y1, x2 - x1, y2 - y1);
 	}
