b3646d0b7513ffb1f2c597e64d5063a16af5a3a8
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-13 22:10:30 -0600

    Increased update speed. Removed yvelocity limit

--- src/view/MyThread.java
+++ src/view/MyThread.java
@@ -7,7 +7,7 @@ class MyThread extends Thread {
 	private GamePanel panel;
 	private MainModel model;
 
-	static final long minimumSleepTime = 33;
+	static final long minimumSleepTime = 21;
 
 	public MyThread(GamePanel gp, MainModel model) {
 		this.panel = gp;
