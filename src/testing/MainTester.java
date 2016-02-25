f647cc1126ed19769e5a6642164c9e593ad09cab
Karl <37496339+aquarial@users.noreply.github.com>
2016-02-25 13:45:49 -0600

    Starting main menu. Testing.Java setup to test menu

--- src/testing/MainTester.java
+++ src/testing/MainTester.java
@@ -1,32 +1,10 @@
 package testing;
 
-import game.GamePanel;
-import game.JPanelConstructor;
-
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import mainmenu.MainMenu;
 
 public class MainTester {
 
 	public static void main(String[] args) {
-
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				int width = 800;
-				int height = 600;
-
-				JFrame f = new JFrame("Demo");
-				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				f.setSize(width, height);
-				f.setResizable(false);
-				f.setVisible(true);
-				f.validate();
-				GamePanel gp = JPanelConstructor.makeNewGamePanel("001", width,
-						height);
-				f.add(gp);
-				gp.startlevel();
-			}
-		});
-
+		new MainMenu();
 	}
 }
