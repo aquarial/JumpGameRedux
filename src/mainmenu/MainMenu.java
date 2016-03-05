9240331719df393b8c0214154e4e5e0c1844750d
Karl <37496339+aquarial@users.noreply.github.com>
2016-03-04 20:59:01 -0600

    Main menu
    Changed to absolute layout. It fits my purposes.
    
    Comments to structure file
    
    
    Adds button and combobox

--- src/mainmenu/MainMenu.java
+++ src/mainmenu/MainMenu.java
@@ -1,19 +1,28 @@
 package mainmenu;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
 import javax.swing.SwingUtilities;
 
 public class MainMenu {
-	private JPanel LevelSelectPanel;
+
+	final static String N = SpringLayout.NORTH;
+	final static String E = SpringLayout.EAST;
+	final static String W = SpringLayout.WEST;
+	final static String S = SpringLayout.SOUTH;
+	private JComboBox<String> levelSelecter;
 
 	public MainMenu() {
+
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				int width = 800;
@@ -23,46 +32,46 @@ public class MainMenu {
 				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				f.setSize(width, height);
 				f.setResizable(false);
-				SpringLayout springLayout = new SpringLayout();
-				f.getContentPane().setLayout(springLayout);
+				f.getContentPane().setLayout(null);
+
+				//
+				// *******LEFT HAND SIDE
+				JPanel LevelSelectPanel = new JPanel();
+				LevelSelectPanel.setBounds(5, 5, 145, 565);
+				LevelSelectPanel.setBackground(Color.BLUE);
+				f.getContentPane().add(LevelSelectPanel);
+				DefaultListModel<String> dlm = new DefaultListModel<>();
+				dlm.addElement("001");
+				for (int i = 0; i < 100; i++) {
+					dlm.addElement("" + i);
+				}
+
+				// 1st element - "Play Level" Button
+				JButton startLevelButton = new JButton("Play Level");
+				startLevelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("Selected: " + levelSelecter.getSelectedItem());
+					}
+				});
+				LevelSelectPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+				LevelSelectPanel.add(startLevelButton);
+
+				// 2nd element - "Select level" combo box
+				levelSelecter = new JComboBox<>();
+				LevelSelectPanel.add(levelSelecter);
+
+				//
+				// ********MIDDLE BOX
+				JPanel levelInfoPanel = new JPanel();
+				levelInfoPanel.setBounds(155, 5, 635, 565);
+				levelInfoPanel.setBackground(Color.GREEN);
+				f.getContentPane().add(levelInfoPanel);
+				levelInfoPanel.setLayout(null);
 
 				// GamePanel backgroundPanel =
 				// JPanelConstructor.makeNewGamePanel("001");
 				// backgroundPanel.startlevel();
 				// f.getContentPane().add(backgroundPanel, BorderLayout.CENTER);
-
-				LevelSelectPanel = new JPanel();
-				springLayout.putConstraint(SpringLayout.WEST, LevelSelectPanel, 5, SpringLayout.WEST,
-						f.getContentPane());
-				springLayout.putConstraint(SpringLayout.EAST, LevelSelectPanel, 150, SpringLayout.WEST,
-						f.getContentPane());
-				springLayout.putConstraint(SpringLayout.NORTH, LevelSelectPanel, 5, SpringLayout.NORTH,
-						f.getContentPane());
-				springLayout.putConstraint(SpringLayout.SOUTH, LevelSelectPanel, -5, SpringLayout.SOUTH,
-						f.getContentPane());
-				LevelSelectPanel.setBackground(Color.BLUE);
-				f.getContentPane().add(LevelSelectPanel);
-				LevelSelectPanel.setLayout(null);
-
-				JList<String> list = new JList<>();
-				list.setBounds(12, 12, 125, 400);
-				DefaultListModel<String> dlm = new DefaultListModel<>();
-				dlm.addElement("001");
-				list.setModel(dlm);
-				list.setSelectedIndex(0);
-				LevelSelectPanel.add(list);
-
-				JPanel panel = new JPanel();
-				springLayout.putConstraint(SpringLayout.NORTH, panel, 5, SpringLayout.NORTH, f.getContentPane());
-				springLayout.putConstraint(SpringLayout.WEST, panel, 5, SpringLayout.EAST, LevelSelectPanel);
-
-				JButton btnNewButton = new JButton("New button");
-				btnNewButton.setBounds(12, 429, 127, 36);
-				LevelSelectPanel.add(btnNewButton);
-				springLayout.putConstraint(SpringLayout.SOUTH, panel, -5, SpringLayout.SOUTH, f.getContentPane());
-				springLayout.putConstraint(SpringLayout.EAST, panel, -5, SpringLayout.EAST, f.getContentPane());
-				panel.setBackground(Color.GREEN);
-				f.getContentPane().add(panel);
 
 				f.setVisible(true);
 				f.validate();
