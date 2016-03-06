e04a0c0c90aba1756544807432e3556fdcff3b7b
Karl <37496339+aquarial@users.noreply.github.com>
2016-03-05 22:53:07 -0600

    No MenuView.
    The logic is simple enough that I don't need a model-view-controller
    pattern. Simply adding the controls in the main class is clear enough

--- src/mainmenu/MainMenu.java
+++ src/mainmenu/MainMenu.java
@@ -1,24 +1,66 @@
 package mainmenu;
 
-import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import mainmenu.game.GamePanel;
+import mainmenu.levelselect.SelectPanel;
+import mainmenu.splashscreen.SplashPanel;
 
 public class MainMenu {
 
-	private MenuView view;
-	private MenuState state;
+	private JPanel contentPane;
+	private SplashPanel splashpanel = new SplashPanel();
+	private SelectPanel selectpanel = new SelectPanel();
+	private GamePanel gamepanel = new GamePanel();
 
 	public MainMenu() {
-		SwingUtilities.invokeLater(new Runnable() {
+		int width = 800;
+		int height = 600;
 
-			@Override
-			public void run() {
-				state = MenuState.LEVEL_SELECT;
-				view = new MenuView();
+		JFrame f = new JFrame("Demo");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(width, height);
+		f.setResizable(false);
+		contentPane = (JPanel) f.getContentPane();
+		contentPane.setLayout(new BorderLayout());
 
-				view.initState(state);
-
-			}
-		});
+		f.setVisible(true);
+		f.validate();
+		
+		
+		initState(MenuState.LEVEL_SELECT);
 	}
+
+	/**
+	 * Resets the menu. Sets up parts ready for the state provided
+	 * 
+	 * @param newstate
+	 */
+	void initState(MenuState newstate) {
+		contentPane.removeAll();
+		switch (newstate) {
+		case SPLASH_SCREEN:
+			contentPane.add(splashpanel, BorderLayout.CENTER);
+			break;
+		case LEVEL_SELECT:
+			contentPane.add(selectpanel, BorderLayout.CENTER);
+			break;
+		case PLAY_GAME:
+			gamepanel.startlevel("001");
+			contentPane.add(gamepanel, BorderLayout.CENTER);
+			break;
+		default:
+			break;
+		}
+		contentPane.revalidate();
+		contentPane.repaint();
+	}
+	
+	
+	
+	
 
 }
--- src/testing/MainTester.java
+++ src/testing/MainTester.java
@@ -1,19 +1,17 @@
 package testing;
 
+import javax.swing.SwingUtilities;
+
 import mainmenu.MainMenu;
 
 public class MainTester {
 
 	public static void main(String[] args) {
-		new MainMenu();
-		// JFrame f = new JFrame();
-		// f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// f.setSize(800, 600);
-		// f.setVisible(true);
-		// GamePanel gp = JPanelConstructor.makeNewGamePanel("001");
-		// gp.startlevel();
-		// f.getContentPane().add(gp);
-		// f.revalidate();
-		// f.repaint();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new MainMenu();
+			}
+		});
 	}
 }
