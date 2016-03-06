0965cfdbb8e94911ea9b6879154ee516af94ff6b
Karl <37496339+aquarial@users.noreply.github.com>
2016-03-05 21:26:28 -0600

    Test main menu

--- src/testing/MainTester.java
+++ src/testing/MainTester.java
@@ -1,23 +1,19 @@
 package testing;
 
-import javax.swing.JFrame;
-
-import mainmenu.JPanelConstructor;
 import mainmenu.MainMenu;
-import mainmenu.game.GamePanel;
 
 public class MainTester {
 
 	public static void main(String[] args) {
-		// new MainMenu();
-		JFrame f = new JFrame();
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(800, 600);
-		f.setVisible(true);
-		GamePanel gp = JPanelConstructor.makeNewGamePanel("001");
-		gp.startlevel();
-		f.getContentPane().add(gp);
-		f.revalidate();
-		f.repaint();
+		new MainMenu();
+		// JFrame f = new JFrame();
+		// f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// f.setSize(800, 600);
+		// f.setVisible(true);
+		// GamePanel gp = JPanelConstructor.makeNewGamePanel("001");
+		// gp.startlevel();
+		// f.getContentPane().add(gp);
+		// f.revalidate();
+		// f.repaint();
 	}
 }
