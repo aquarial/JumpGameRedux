4f1396a8956ae2974bc5692ba530cd23fed8bdfb
Karl <37496339+aquarial@users.noreply.github.com>
2016-03-26 10:17:30 -0500

    Constructor works correctly now

--- src/mainmenu/game/GamePanel.java
+++ src/mainmenu/game/GamePanel.java
@@ -39,15 +39,13 @@ public class GamePanel extends JPanel {
 		endGame = goToSelectPanel;
 	}
 
-	public GamePanel() {
-		addClickToJumpListener();
-		bufferedImage = new BufferedImage(xcenter * 2, ycenter * 2, BufferedImage.TYPE_INT_RGB);
-		graphicsForBufferedImage = (Graphics2D) bufferedImage.getGraphics();
-	}
-
 	public GamePanel(int width, int height) {
+		addClickToJumpListener();
+
 		xcenter = width / 2;
 		ycenter = height / 2;
+		bufferedImage = new BufferedImage(xcenter * 2, ycenter * 2, BufferedImage.TYPE_INT_RGB);
+		graphicsForBufferedImage = (Graphics2D) bufferedImage.getGraphics();
 	}
 
 	public void startlevel(String levelname) {
