1c6580e6f22be7ceadc648f2641b66c34b9c5c64
Karl <37496339+aquarial@users.noreply.github.com>
2016-03-29 21:55:16 -0500

    Minor change to splashscreen length

--- src/mainmenu/MainMenu.java
+++ src/mainmenu/MainMenu.java
@@ -90,7 +90,7 @@ public class MainMenu {
 			public void run() {
 				try {
 
-					Thread.sleep(4000);
+					Thread.sleep(2000);
 					int reps = 200;
 					for (int i = 0; i < reps; i++) {
 						Thread.sleep(1000 / reps);
