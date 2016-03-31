cb3ef0602ef8fa0c87b1cb4f6b36e94a1576ba29
Karl <37496339+aquarial@users.noreply.github.com>
2016-03-31 15:06:18 -0500

    Add some useful methods.

--- src/mainmenu/levelselect/savestates/GameSave.java
+++ src/mainmenu/levelselect/savestates/GameSave.java
@@ -1,6 +1,8 @@
 package mainmenu.levelselect.savestates;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import io.Resources;
@@ -13,7 +15,7 @@ import io.Resources;
 public class GameSave {
 
     Map<String, LevelRecord> levelrecords;
-    
+
     /**
      * Creates an empty game save
      */
@@ -23,6 +25,37 @@ public class GameSave {
             String withoutEnding = name.substring(0, name.length() - 4);
             levelrecords.put(withoutEnding, new LevelRecord(withoutEnding));
         }
+    }
+
+    /**
+     * @return List of unlocked levels
+     */
+    public List<String> getUnlockedLevels() {
+        List<String> unlockedlevels = new ArrayList<String>();
+        for (Map.Entry<String, LevelRecord> entry : levelrecords.entrySet()) {
+            if (entry.getValue().isUnlocked()) {
+                unlockedlevels.add(entry.getKey());
+            }
+        }
+        Collections.sort(unlockedlevels);
+        return unlockedlevels;
+    }
+
+    /**
+     * @param levelname
+     * @return Whether this game save has unlocked a level
+     */
+    public boolean isLevelUnlocked(String levelname) {
+        return levelrecords.get(levelname).isUnlocked();
+    }
+
+    /**
+     * Sets a level to unlocked by name
+     * 
+     * @param levelname
+     */
+    public void unlockALevel(String levelname) {
+        levelrecords.get(levelname).setUnlocked(true);
     }
 
 }
