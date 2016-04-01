c5817b6f16ea4f69c379e15d1cbf9dc2c35376b2
Karl <37496339+aquarial@users.noreply.github.com>
2016-03-31 20:28:42 -0500

    Ctrl-Shift-F format

--- src/mainmenu/game/GamePanel.java
+++ src/mainmenu/game/GamePanel.java
@@ -24,130 +24,127 @@ import mainmenu.game.model.block.BlockType;
  */
 public class GamePanel extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-	private static int xcenter;
-	private static int ycenter;
+    private static final long serialVersionUID = 1L;
+    private static int xcenter;
+    private static int ycenter;
 
-	private BufferedImage bufferedImage;
-	private Graphics2D graphicsForBufferedImage;
+    private BufferedImage bufferedImage;
+    private Graphics2D graphicsForBufferedImage;
     private MainModel model;
 
-	RunCode endGame;
+    RunCode endGame;
 
-	public void setOnFinish(RunCode goToSelectPanel) {
-		endGame = goToSelectPanel;
-	}
+    public void setOnFinish(RunCode goToSelectPanel) {
+        endGame = goToSelectPanel;
+    }
 
-	public GamePanel(int width, int height) {
-		addClickListener();
-		setBackground(Color.WHITE);
+    public GamePanel(int width, int height) {
+        addClickListener();
+        setBackground(Color.WHITE);
 
-		xcenter = width / 2;
-		ycenter = height / 2;
-		bufferedImage = new BufferedImage(width, height,
-				BufferedImage.TYPE_INT_RGB);
-		graphicsForBufferedImage = (Graphics2D) bufferedImage.getGraphics();
-	}
+        xcenter = width / 2;
+        ycenter = height / 2;
+        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        graphicsForBufferedImage = (Graphics2D) bufferedImage.getGraphics();
+    }
 
-	public void startlevel(String levelname) {
-		model = new MainModel(levelname);
-		Thread th = new GameThread(this, model);
-		th.start();
-	}
-	
-	@Override
-	public void paintComponent(Graphics g) {
-		Graphics2D g2 = graphicsForBufferedImage;
+    public void startlevel(String levelname) {
+        model = new MainModel(levelname);
+        Thread th = new GameThread(this, model);
+        th.start();
+    }
 
-		g2.setColor(Color.WHITE);
-		g2.fillRect(0, 0, getWidth(), getHeight());
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = graphicsForBufferedImage;
 
-		g2.setColor(Color.BLUE);
-		this.drawPlayerAtCenter(g2);
+        g2.setColor(Color.WHITE);
+        g2.fillRect(0, 0, getWidth(), getHeight());
 
-		g2.setColor(new Color(235, 255, 235));
-		g2.drawOval(xcenter - 200, ycenter - 200, 400, 400);
-		g2.drawOval(xcenter - 100, ycenter - 100, 200, 200);
+        g2.setColor(Color.BLUE);
+        this.drawPlayerAtCenter(g2);
 
-		for (Block stickyQuad : model.getBlockData()) {
+        g2.setColor(new Color(235, 255, 235));
+        g2.drawOval(xcenter - 200, ycenter - 200, 400, 400);
+        g2.drawOval(xcenter - 100, ycenter - 100, 200, 200);
 
-			if (stickyQuad.getBLockType() == BlockType.STICKY) {
-				g2.setColor(Color.BLACK);
-			}
-			if (stickyQuad.getBLockType() == BlockType.FINISH) {
-				g2.setColor(Color.CYAN);
-			}
-			drawStickyQuad(stickyQuad, g2);
-		}
+        for (Block stickyQuad : model.getBlockData()) {
 
-		g.drawImage(bufferedImage, 0, 0, null);
-	}
+            if (stickyQuad.getBLockType() == BlockType.STICKY) {
+                g2.setColor(Color.BLACK);
+            }
+            if (stickyQuad.getBLockType() == BlockType.FINISH) {
+                g2.setColor(Color.CYAN);
+            }
+            drawStickyQuad(stickyQuad, g2);
+        }
 
-	/**
-	 * Translates the model coordinates to screen coordinates, and fills in the
-	 * pixel rectangle at that location
-	 * 
-	 * @param stickyQuad
-	 * @param g2
-	 */
-	private void drawStickyQuad(Block stickyQuad, Graphics2D g2) {
-		int x1 = modelUnitToPixels(stickyQuad.getX1() - model.getPlayerXPos());
-		int y1 = modelUnitToPixels(model.getPlayerYPos() - stickyQuad.getY2());
-		int x2 = modelUnitToPixels(stickyQuad.getX2() - model.getPlayerXPos());
-		int y2 = modelUnitToPixels(model.getPlayerYPos() - stickyQuad.getY1());
+        g.drawImage(bufferedImage, 0, 0, null);
+    }
 
-		g2.fillRect(xcenter + x1, ycenter + y1, x2 - x1, y2 - y1);
-	}
+    /**
+     * Translates the model coordinates to screen coordinates, and fills in the
+     * pixel rectangle at that location
+     * 
+     * @param stickyQuad
+     * @param g2
+     */
+    private void drawStickyQuad(Block stickyQuad, Graphics2D g2) {
+        int x1 = modelUnitToPixels(stickyQuad.getX1() - model.getPlayerXPos());
+        int y1 = modelUnitToPixels(model.getPlayerYPos() - stickyQuad.getY2());
+        int x2 = modelUnitToPixels(stickyQuad.getX2() - model.getPlayerXPos());
+        int y2 = modelUnitToPixels(model.getPlayerYPos() - stickyQuad.getY1());
 
-	/**
-	 * 
-	 * @param g2
-	 */
-	private void drawPlayerAtCenter(Graphics2D g2) {
-		int playerRad = modelUnitToPixels(model.getPlayerWidth() / 2);
-		g2.fillRect(xcenter - playerRad, ycenter - playerRad, playerRad * 2,
-				playerRad * 2);
+        g2.fillRect(xcenter + x1, ycenter + y1, x2 - x1, y2 - y1);
+    }
 
-	}
+    /**
+     * 
+     * @param g2
+     */
+    private void drawPlayerAtCenter(Graphics2D g2) {
+        int playerRad = modelUnitToPixels(model.getPlayerWidth() / 2);
+        g2.fillRect(xcenter - playerRad, ycenter - playerRad, playerRad * 2, playerRad * 2);
 
-	/**
-	 * Scale from model to pixels
-	 * 
-	 * @param unit
-	 * @return
-	 */
-	private int modelUnitToPixels(double unit) {
-		return (int) (unit * 25);
-	}
+    }
 
-	/**
-	 * 
-	 * Adds mouseListener to <code>this</code>
-	 * <p>
-	 * Clears up constructor
-	 * 
-	 */
-	private void addClickListener() {
-		// Jump Listener
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent e) {
-				double xdiff = e.getX() - xcenter;
-				double ydiff = e.getY() - ycenter;
+    /**
+     * Scale from model to pixels
+     * 
+     * @param unit
+     * @return
+     */
+    private int modelUnitToPixels(double unit) {
+        return (int) (unit * 25);
+    }
 
-				double angle = Math.atan2(ydiff, xdiff);
-				double power = calculatePowerFromDiffs(xdiff, ydiff);
+    /**
+     * 
+     * Adds mouseListener to <code>this</code>
+     * <p>
+     * Clears up constructor
+     * 
+     */
+    private void addClickListener() {
+        // Jump Listener
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                double xdiff = e.getX() - xcenter;
+                double ydiff = e.getY() - ycenter;
 
-				model.addJumpPowerToJumper(angle, power);
-			}
+                double angle = Math.atan2(ydiff, xdiff);
+                double power = calculatePowerFromDiffs(xdiff, ydiff);
 
-			private double calculatePowerFromDiffs(double xdiff, double ydiff) {
-				double normalPower = Math
-						.sqrt(Math.pow(xdiff, 2) + Math.pow(ydiff, 2));
-				return Math.min(normalPower / 18, 7.5);
-			}
+                model.addJumpPowerToJumper(angle, power);
+            }
 
-		});
-	}
+            private double calculatePowerFromDiffs(double xdiff, double ydiff) {
+                double normalPower = Math.sqrt(Math.pow(xdiff, 2) + Math.pow(ydiff, 2));
+                return Math.min(normalPower / 18, 7.5);
+            }
+
+        });
+    }
 
 }
