669a3a40da92a246b0561e7b57847809631d8e7d
Karl <37496339+aquarial@users.noreply.github.com>
2016-04-22 13:22:04 -0500

    LevelRecord has data about its level.
    updateRecords updates it with the completed level.

--- src/mainmenu/savestates/LevelRecord.java
+++ src/mainmenu/savestates/LevelRecord.java
@@ -1,10 +1,15 @@
 package mainmenu.savestates;
 
+import mainmenu.game.model.ModelHistory;
+
 /**
  * Represents how
  *
  */
 public class LevelRecord {
+
+    ModelHistory fastestTime;
+    ModelHistory leastJumps;
 
     LevelRecord(String levelname) {
 
@@ -12,4 +17,23 @@ public class LevelRecord {
 
     // Getters & setters
 
+    /**
+     * Updates the records with recently completed level
+     * 
+     * @param history
+     *            The completed level history
+     */
+    void updateRecords(ModelHistory history) {
+        if (history.getPlayerFinished()) {
+
+            if (fastestTime == null || history.getTimeOfFinish() < fastestTime.getTimeOfFinish()) {
+                fastestTime = history;
+            }
+
+            if (leastJumps == null || history.getNumberOfJumpsSoFar() < leastJumps.getNumberOfJumpsSoFar()) {
+                leastJumps = history;
+            }
+
+        }
+    }
 }
