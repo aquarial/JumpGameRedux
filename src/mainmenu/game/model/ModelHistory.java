b1ed216fe68a53a06736ee435149b05781627b5b
Karl <37496339+aquarial@users.noreply.github.com>
2016-04-22 12:23:07 -0500

    ModelHistory upgraded.
    ModelHistory now knows:
    1 if the player finished the level
    2 how long it took
    3 how long it's been at any point since the begining
    4 how many jumps they took
    5 how many jumps since the begining
    
    Now I return ModelHistory MainModel, and let that go all the way up to
    the MainMenu, into the level select data

--- src/mainmenu/game/model/ModelHistory.java
+++ src/mainmenu/game/model/ModelHistory.java
@@ -7,18 +7,38 @@ public class ModelHistory {
 
     private String levelname;
     private List<double[]> jumpHistory;
+    private double startTime;
+    private double finishTime;
 
     public ModelHistory(String level_name) {
         levelname = level_name;
         jumpHistory = new ArrayList<>();
+        startTime = System.currentTimeMillis();
+        finishTime = -1;
     }
 
     public String getLevelName() {
         return levelname;
     }
 
+    public void setFinishTime() {
+        finishTime = System.currentTimeMillis();
+    }
+
+    public double getTimeFromStart() {
+        return (System.currentTimeMillis() - startTime) / 1000.0;
+    }
+
+    public double getTimeOfFinish() {
+        return (finishTime - startTime) / 1000.0;
+    }
+
     public void addToJumpHistory(double[] new_data) {
         jumpHistory.add(new_data);
+    }
+
+    public int getNumberOfJumpsSoFar() {
+        return jumpHistory.size();
     }
 
     public List<double[]> getJumpHistory() {
