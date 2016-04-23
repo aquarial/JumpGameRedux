bd9ee9caaf733fa0b6b869e527bc30916de8ea77
Karl <37496339+aquarial@users.noreply.github.com>
2016-04-22 20:16:53 -0500

    Refactor SelectPanel to use AbsoluteLayout.
    I need this because I will be drawing scaled iamges to the panel. A
    known, constant size is a must.

--- src/mainmenu/levelselect/SelectPanel.java
+++ src/mainmenu/levelselect/SelectPanel.java
@@ -1,6 +1,5 @@
 package mainmenu.levelselect;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
@@ -22,30 +21,35 @@ public class SelectPanel extends JPanel {
     /**
      * Constructs all of the components that make up the level select panel
      */
-    public SelectPanel() {
+    public SelectPanel(int width, int height) {
 
-        this.setLayout(new BorderLayout());
+        setLayout(null);
+
         // *******LEFT HAND SIDE
-        JPanel LevelSelectPanel = new JPanel();
-        LevelSelectPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-        LevelSelectPanel.setBackground(Color.BLUE);
+        JPanel levelSelectPanel = new JPanel();
+        levelSelectPanel.setBounds(0, 0, 130, height);
+        levelSelectPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        levelSelectPanel.setBackground(Color.BLUE);
 
-        // 1st element - "Play Level" Button
-        startLevelButton = new JButton("Start Level");
-        LevelSelectPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-        LevelSelectPanel.add(startLevelButton);
-
-        // 2nd element - "Select level" combo box
+        // 1st element - "Select level" combo box
         levelSelecter = new JComboBox<>();
-        LevelSelectPanel.add(levelSelecter);
+        levelSelecter.setToolTipText("Choose level to start");
+        levelSelectPanel.add(levelSelecter);
+
+        // 2nd element - "Play Level" Button
+        startLevelButton = new JButton("Start Level");
+        startLevelButton.setToolTipText("Start selected level");
+        levelSelectPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        levelSelectPanel.add(startLevelButton);
 
         //
         // ********MIDDLE BOX
-        JPanel levelInfoPanel = new JPanel();
+        LevelInfoPanel levelInfoPanel = new LevelInfoPanel();
+        levelInfoPanel.setBounds(130, 0, width - 130, height);
         levelInfoPanel.setBackground(Color.GREEN);
 
-        add(LevelSelectPanel, BorderLayout.WEST);
-        add(levelInfoPanel, BorderLayout.CENTER);
+        add(levelSelectPanel);
+        add(levelInfoPanel);
 
     }
 
