d5eae08b1acb7a1f110b2adfe2afe7c3d4e179a9
Karl <37496339+aquarial@users.noreply.github.com>
2016-04-24 14:33:40 -0500

    Label positioning with lots of magic values

--- src/mainmenu/levelselect/LevelInfoPanel.java
+++ src/mainmenu/levelselect/LevelInfoPanel.java
@@ -5,16 +5,19 @@ import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import io.Resources;
 
 public class LevelInfoPanel extends JPanel {
     private static final long serialVersionUID = 1L;
     private static final int BORDER_BUFFER = 55;// px
-
+    private static final int labelHeight = 4343; // px
     private int width;
 
-    private JLabel label_levelimage;
+    private JLabel lLevelimage;
+    private JLabel lLeastjumpsText;
+    private JLabel lFastestTimeText;
 
     /**
      * @param width
@@ -28,9 +31,19 @@ public class LevelInfoPanel extends JPanel {
 
         int IMAGE_WIDTH = width - 2 * BORDER_BUFFER;
         int IMAGE_HEIGHT = (int) (.65 * IMAGE_WIDTH);
-        label_levelimage = new JLabel();
-        label_levelimage.setBounds(BORDER_BUFFER, BORDER_BUFFER, IMAGE_WIDTH, IMAGE_HEIGHT);
-        this.add(label_levelimage);
+
+        lLevelimage = new JLabel();
+        lLevelimage.setBounds(BORDER_BUFFER, BORDER_BUFFER, IMAGE_WIDTH, IMAGE_HEIGHT);
+        this.add(lLevelimage);
+
+        lLeastjumpsText = new JLabel();
+        lLeastjumpsText.setBounds(100, 450, 200, 80);
+        this.add(lLeastjumpsText);
+
+        lFastestTimeText = new JLabel();
+        lFastestTimeText.setBounds(440, 450, 200, 80);
+        this.add(lFastestTimeText);
+        System.out.println(height);
     }
 
     /**
@@ -42,10 +55,10 @@ public class LevelInfoPanel extends JPanel {
     void setupPanelForLevel(String levelname) {
         BufferedImage tmp = Resources.getImageForLevel(levelname);
         tmp = Resources.scaleImage(tmp, width - 2 * BORDER_BUFFER);
-        label_levelimage.setIcon(new ImageIcon(tmp));
+        lLevelimage.setIcon(new ImageIcon(tmp));
 
-        // TODO add display for current records
-
+        lLeastjumpsText.setText("Least Jumps " + "999");
+        lFastestTimeText.setText("Fastest Time " + "99:99");
     }
 
 }
