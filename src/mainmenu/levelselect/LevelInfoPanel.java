24c1c98c7f4f7f63375c22850badb1abbdf77c70
Karl <37496339+aquarial@users.noreply.github.com>
2016-04-24 14:16:51 -0500

    Renamed label variable

--- src/mainmenu/levelselect/LevelInfoPanel.java
+++ src/mainmenu/levelselect/LevelInfoPanel.java
@@ -14,7 +14,7 @@ public class LevelInfoPanel extends JPanel {
 
     private int width;
 
-    private JLabel l_levelimage;
+    private JLabel label_levelimage;
 
     /**
      * @param width
@@ -28,9 +28,9 @@ public class LevelInfoPanel extends JPanel {
 
         int IMAGE_WIDTH = width - 2 * BORDER_BUFFER;
         int IMAGE_HEIGHT = (int) (.65 * IMAGE_WIDTH);
-        l_levelimage = new JLabel();
-        l_levelimage.setBounds(BORDER_BUFFER, BORDER_BUFFER, IMAGE_WIDTH, IMAGE_HEIGHT);
-        this.add(l_levelimage);
+        label_levelimage = new JLabel();
+        label_levelimage.setBounds(BORDER_BUFFER, BORDER_BUFFER, IMAGE_WIDTH, IMAGE_HEIGHT);
+        this.add(label_levelimage);
     }
 
     /**
@@ -42,7 +42,7 @@ public class LevelInfoPanel extends JPanel {
     void setupPanelForLevel(String levelname) {
         BufferedImage tmp = Resources.getImageForLevel(levelname);
         tmp = Resources.scaleImage(tmp, width - 2 * BORDER_BUFFER);
-        l_levelimage.setIcon(new ImageIcon(tmp));
+        label_levelimage.setIcon(new ImageIcon(tmp));
 
         // TODO add display for current records
 
