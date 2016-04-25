7228ec7a7aad71aee4c4f5aed958fe9c96e9a246
Karl <37496339+aquarial@users.noreply.github.com>
2016-04-24 21:16:18 -0500

    Remove unused imports

--- src/mainmenu/levelselect/SelectPanel.java
+++ src/mainmenu/levelselect/SelectPanel.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.util.List;
 
 import javax.swing.JButton;
