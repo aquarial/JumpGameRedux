93a6280881ffd2e567696e1bbccbdaefacbde616
Karl <37496339+aquarial@users.noreply.github.com>
2017-01-24 16:57:08 -0600

    paintcomponent -> print

--- src/mainmenu/levelselect/SelectPanel.java
+++ src/mainmenu/levelselect/SelectPanel.java
@@ -106,7 +106,7 @@ public class SelectPanel extends JPanel {
     public BufferedImage createImage() {
         BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = bi.createGraphics();
-        this.paintComponent(g);
+        this.print(g);
         g.setColor(Color.MAGENTA);
         g.fillRect(0, 0, 100, 100);
         return bi;
